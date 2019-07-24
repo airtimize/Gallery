7580b7499ce47ff4c0bc71f39a0f73ae222b2c2d
Chad Archila <chad@Chads-MacBook-Pro.local>
2019-07-23 19:18:25 -0700

    Change port #

--- server/index.js
+++ server/index.js
@@ -5,12 +5,20 @@ const controllers = require('./controllers/index');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 3003;
+const port = process.env.PORT || 3012;
 
 const db = require('../database/index.js');
 
 app.use('/rooms/:listingid', express.static(path.join(__dirname, '../public')));
 
 app.get('/api/:listingid/images', cors(), controllers.getImages);
+
+app.post('/api/:listingid/images',);
+
+app.put('/api/:listingid/images',);
+
+app.delete('/api/:listingid/images',);
+
+
 
 app.listen(port, () => { console.log(`Listening on port ${port}`); });
