255ade047fd1ab20042aed7491efa700e391563c
Chad Archila <chad@Chads-MacBook-Pro.local>
2019-08-01 15:42:12 -0700

    Add comments to code

--- client/src/components/Gallery.jsx
+++ client/src/components/Gallery.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import Image from './Image';
 import style from '../style.css';
 
+// Shouldn't need to make any changes here. Once the state gets the correct data/structure, everything else should be normal.
+
 const Gallery = (props) => {
   const { imageClickHandler, imageHoverHandler, imageUnHoverHandler } = props;
-  return props.images.map((image, index) => {
-    const { ImageID, ImageUrl, Caption, Verified, hoverClass } = image;
+  return props.images.map((image, index) => { // map through the prop images which is an array of objects // image = an obj
+    const { ImageID, ImageUrl, Caption, Verified, hoverClass } = image; // destructure obj img. So when imageID is called, it's actually image.ImageID
     const cssId = props.id || style.galleryImage + index;
     return (index < 4) ? (
       <div id={cssId}>
