8c6cbdec9ea36317627bd7a39bc2c8fb4ae6d5ae
Chad Archila <chad@Chads-MacBook-Pro.local>
2019-08-09 15:29:17 -0700

    Configure redis connection

--- server/server/model.js
+++ server/server/model.js
@@ -14,6 +14,17 @@ client.on('error', (err) => {
   console.log('Error ' + err);
 });
 
+client.set("string key", "string val", redis.print);
+client.hset("hash key", "hashtest 1", "some value", redis.print);
+client.hset(["hash key", "hashtest 2", "some other value"], redis.print);
+client.hkeys("hash key", function (err, replies) {
+    console.log(replies.length + " replies:");
+    replies.forEach(function (reply, i) {
+        console.log("    " + i + ": " + reply);
+    });
+    client.quit();
+});
+
 const pool = new Pool(config);
 const getData = (req, res) => {
   const queryStr = `SELECT * FROM images WHERE listing_id = ${req.params.listingid};`;
