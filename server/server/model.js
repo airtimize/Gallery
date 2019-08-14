b3b06453a1a1adfa8da63c7bc3f1b773ea29b7f5
Chad Archila <chad@Chads-MacBook-Pro.local>
2019-08-14 10:48:20 -0700

    Remove broken redis query

--- server/server/model.js
+++ server/server/model.js
@@ -1,10 +1,5 @@
-const Pool = require('pg').Pool;
-const redis = require('redis');
-const bluebird = require('bluebird');
+const { Pool } = require('pg');
 
-const client = redis.createClient();
-bluebird.promisifyAll(redis.RedisClient.prototype);
-bluebird.promisifyAll(redis.Multi.prototype);
 
 const config = {
   user: 'power_user',
@@ -13,48 +8,22 @@ const config = {
   database: 'sdc',
 };
 
-client.on('error', (err) => {
-  console.log(`Error ${err}`);
-});
-
-client.set('string key', 'string val', redis.print);
-client.hset('hash key', 'hashtest 1', 'some value', redis.print);
-client.hset(['hash key', 'hashtest 2', 'some other value'], redis.print);
-client.hkeys('hash key', function (err, replies) {
-  console.log(replies.length + ' replies:');
-  replies.forEach(function (reply, i) {
-      console.log('    ' + i + ': ' + reply);
-  });
-  // client.quit();
-});
-
 const pool = new Pool(config);
 
-const getData = (req, res) => client.getAsync(req.params.listingid)
-  .then((success) => {
-    // need to figure out how to get this to send info back to the front end.
+const getData = (req, res) => {
+  const queryStr = `
+    SELECT * FROM images
+      WHERE listing_id = ${req.params.listingid};
+  `;
+
+  pool.query(queryStr, (err, success) => {
+    if (err) {
+      res.status(404).send(err);
+      return;
+    }
     res.status(200).json(success.rows);
-    console.log('redis success2');
-  /* CA: when an image is already in redis, it doesn't work.
-  If an image is not in redis then it goes to the catch and
-  find the image in postgres and add it to redis.
-  To make it work for an image already in redit remove the err handling from catch.
-  We need to figure out how to get the image data
-  sent back to the client when the data is already in redis. */
-  })
-  .catch(() => {
-    const queryStr = `SELECT * FROM images WHERE listing_id = ${req.params.listingid};`;
-    pool.query(queryStr, (err, success) => {
-      if (err) {
-        res.status(404).send(err);
-        return;
-      }
-
-      client.set(req.params.listingid, JSON.stringify(success.rows));
-      res.status(200).json(success.rows);
-    });
   });
-
+};
 
 const deletePhoto = (req, res) => {
   const queryStr = `delete from images where "ImageID" = ${req.params.listingid};`;
